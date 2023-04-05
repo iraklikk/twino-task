af375754268b948f5ed8e2e2ff5c6c0078d12cd8
iraklikk <iraklikvaratskhelia997@gmail.com>
2023-04-05 17:50:02 +0400

    removed static data from persons service

--- src/app/main/services/persons.service.ts
+++ src/app/main/services/persons.service.ts
@@ -8,30 +8,6 @@ import { BehaviorSubject, Subject } from "rxjs";
 export class PersonsService {
 
   persons: Person[] = [
-    {
-      firstName : "Meri",
-      id: 1,
-      imageSource :    "assets/icons/meri.jpg",
-      income : "3333",
-      lastName : "Robitashvili",
-      score : 1000
-    },
-    {
-      firstName : "Giorgi",
-      id: 2,
-      imageSource :    "assets/icons/meri.jpg",
-      income : "3333",
-      lastName : "Mirashvili",
-      score : 1000
-    },
-    {
-      firstName : "Irakli",
-      id: 3,
-      imageSource : "",
-      income : "3333",
-      lastName : "Kvaratskhelia",
-      score : 1000
-    }
   ];
 
   saveUser$ = new Subject<boolean>();
